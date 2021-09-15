f7c2b286a5e136250db2c59c0e72214f0ea3fb1e
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 19:55:39 +0530

    [go] Sublist iteration 2
    Switch to brute-force algorithm over stdlib’s Rubin-Karp substring
    search algorithm.

--- go/sublist/sublist.go
+++ go/sublist/sublist.go
@@ -1,44 +1,58 @@
 package sublist
 
-import (
-	"fmt"
-	"strings"
-)
-
 type Relation string
-type intSlice []int
 
-// String returns a string representation of []int
-func (l intSlice) String() string {
-	// using a value recevier instead of a pointer receiver since slice copies
-	// are not deep copies:
-	// https://blog.golang.org/go-slices-usage-and-internals
-	var b strings.Builder
-	for _, e := range l {
-		// without the delimiter false negatives pass e.g. [1,23] == [12,3]
-		fmt.Fprintf(&b, "%d,", e)
+func areEqual(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
 	}
-	return b.String()
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
-func Sublist(a, b intSlice) Relation {
-	na, nb := len(a), len(b)
-	sa, sb := a.String(), b.String()
-	// use stdlib’s substring search implementation like KMP that’s way more
-	// tested, robust and perhaps optimized than reinventing the wheel
-	substring := false
-	if na >= nb {
-		substring = strings.Contains(sa, sb)
-	} else {
-		substring = strings.Contains(sb, sa)
-	}
-	if substring {
-		if na == nb {
-			return "equal"
-		} else if na > nb {
-			return "superlist"
+// Brute-force approach since the strings.Contains approach involved memory
+// allocation which makes it a lot slower.  stdlib implements the Rubin–Karp
+// substring search algorithm whose average and best case running time is O(n+m)
+// in space O(p), but its worst-case time is O(nm).  Wikipedia on brute-force:
+// … works well in many practical cases, but can exhibit relatively long running
+// times on certain examples, such as searching for a pattern string of 10,000
+// "a"s followed by a single "b" in a search string of 10 million "a"s, in which
+// case it exhibits its worst-case O(mn) time.
+func index(needle, haystack []int) int {
+	for i := 0; i < len(haystack)-len(needle)+1; i++ {
+		if areEqual(needle, haystack[i:i+len(needle)]) {
+			return i
 		}
-		return "sublist"
 	}
-	return "unequal"
+	return -1
+}
+
+func Sublist(a, b []int) Relation {
+	na, nb := len(a), len(b)
+	if na == 0 && nb == 0 {
+		return "equal"
+	} else if na == 0 {
+		return "sublist"
+	} else if nb == 0 {
+		return "superlist"
+	}
+	idx := -1
+	if na >= nb {
+		idx = index(b, a)
+	} else {
+		idx = index(a, b)
+	}
+	if idx < 0 {
+		return "unequal"
+	}
+	if na == nb {
+		return "equal"
+	} else if na > nb {
+		return "superlist"
+	}
+	return "sublist"
 }

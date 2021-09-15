2e28382b5bc893814ff775e58b31045bebe06aeb
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 19:21:35 +0530

    [go] Bob iteration 2

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,16 +1,31 @@
+// Package bob defines Bob
 package bob
 
 import (
 	"strings"
+	"unicode"
 )
 
 const testVersion = 3
 
+// Hey is the way bob talks
 func Hey(s string) string {
-	s = strings.Trim(s, " \t\n\r")
+	yell := true
+	alpha := false
+	s = strings.Map(func(r rune) rune {
+		if unicode.IsLetter(r) {
+			alpha = true
+		}
+		if unicode.IsSpace(r) {
+			return -1
+		} else if r >= 'a' && r <= 'z' {
+			yell = false
+		}
+		return r
+	}, s)
 	if s == "" {
 		return "Fine. Be that way!"
-	} else if s == strings.ToUpper(s) && strings.ContainsAny(s, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+	} else if alpha && yell {
 		return "Whoa, chill out!"
 	} else if strings.HasSuffix(s, "?") {
 		return "Sure."

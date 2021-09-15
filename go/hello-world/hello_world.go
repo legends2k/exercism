bbf246511e6a16898115ba77ef04b6b024e769ea
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 18:46:29 +0530

    Initial commit
    Repo to version control all Exercism.io workouts as described in
    
    https://medium.com/@JimLynchCodes_76316/how-to-setup-exercism-cli-to-use-your-own-github-repository-5892d401b39f

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,10 @@
+// Package greeting greets the world with a ‘Hello’ message.
+package greeting
+
+
+const testVersion = 4
+
+// HelloWorld returns the greeting string ‘Hello, World!’
+func HelloWorld() string {
+	return "Hello, World!"
+}

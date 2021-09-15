8b328ab1cc8cb451f51152f6114be9daf6319062
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 20:15:32 +0530

    [go] Secret Handshake iteration 2

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -17,20 +17,20 @@ func Handshake(code uint) []string {
 	mask := uint(1)
 	result := make([]string, 0, 4)
 	msgs := [...]string{
-		1: "wink",
-		2: "double blink",
-		4: "close your eyes",
-		8: "jump"}
+		1:  "wink",
+		2:  "double blink",
+		4:  "close your eyes",
+		8:  "jump"}
 	const reverseMsg = 16
 	// fmt.Printf("%b\n", code)
 	for code != 0 {
 		id := code & mask
 		code &^= mask
 		// fmt.Printf("%b\t%b\t%b\n", mask, code, id)
-		if (id != reverseMsg) && (msgs[id] != "") {
-			result = append(result, msgs[id])
-		} else {
+		if id == reverseMsg {
 			reverseSlice(&result)
+		} else if msgs[id] != "" {
+			result = append(result, msgs[id])
 		}
 		mask <<= 1
 		mask |= 1

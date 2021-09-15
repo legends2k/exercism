fcc9c6008716e0a35d920950bc4684f4bc4813be
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 20:19:50 +0530

    [go] Pangram iteration 2

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,9 +1,7 @@
 // Package pangram provides utilities to test for pangrams
 package pangram
 
-import (
-	"unicode"
-)
+import "unicode"
 
 const testVersion = 1
 
@@ -12,13 +10,9 @@ func IsPangram(s string) bool {
 	f := uint32(0x3ffffff) // one bit for each alphabet
 	for _, c := range s {
 		if unicode.IsLetter(c) {
-			// fmt.Printf("%c\t", c)
 			c = unicode.ToLower(c) - 'a'
-			mask := uint32(^(1 << uint(c)))
-			// fmt.Printf("%v\t%b\n", c, mask)
-			f = f & mask
+			f &^= (1 << uint(c))
 		}
 	}
-	// fmt.Printf("%b\n", f)
 	return f == 0
 }

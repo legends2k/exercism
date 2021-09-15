7ada58c99110c9a2b6c38e8b4bc74a0ba0b761b3
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 20:08:59 +0530

    [go][failing] Paasio iteration 2

--- go/paasio/paasio.go
+++ go/paasio/paasio.go
@@ -6,45 +6,64 @@ import (
 )
 
 type CountingReader struct {
-	r    io.Reader
-	n    int64
-	nops int64
+	r io.Reader
+	// Note: pack both byte and op count in one uint64 to use an atomic
+	// instead of a mutex. In some sense, this is cheating since the byte
+	// count itself can be as large as int64.
+	cnt uint64
 }
 
 func (cr *CountingReader) Read(p []byte) (n int, err error) {
 	n, err = cr.r.Read(p)
-	atomic.AddInt64(&cr.n, int64(n))
-	atomic.AddInt64(&cr.nops, 1)
+	comitted := false
+	for !comitted {
+		org_val := atomic.LoadUint64(&cr.cnt)
+		val := org_val
+		val = val + uint64(n)   // lower bits have byte count, simply add!
+		val = val + 0x100000000 // add one to ops
+		comitted = atomic.CompareAndSwapUint64(&cr.cnt, org_val, val)
+	}
 	return
 }
 
 func (cr *CountingReader) ReadCount() (n int64, nops int) {
-	return cr.n, int(cr.nops)
+	cnt := atomic.LoadUint64(&cr.cnt)
+	n = int64(cnt & 0xFFFFFFFF)
+	nops = int(cnt >> 32)
+	return
 }
 
 func NewReadCounter(r io.Reader) ReadCounter {
-	return &CountingReader{r, 0, 0}
+	return &CountingReader{r, 0}
 }
 
 type CountingWriter struct {
-	r    io.Writer
-	n    int64
-	nops int64
+	r   io.Writer
+	cnt uint64
 }
 
 func (cr *CountingWriter) Write(p []byte) (n int, err error) {
 	n, err = cr.r.Write(p)
-	atomic.AddInt64(&cr.n, int64(n))
-	atomic.AddInt64(&cr.nops, 1)
+	comitted := false
+	for !comitted {
+		org_val := atomic.LoadUint64(&cr.cnt)
+		val := org_val
+		val = val + uint64(n)   // lower bits have byte count, simply add!
+		val = val + 0x100000000 // add one to ops
+		comitted = atomic.CompareAndSwapUint64(&cr.cnt, org_val, val)
+	}
 	return
 }
 
 func (cr *CountingWriter) WriteCount() (n int64, nops int) {
-	return cr.n, int(cr.nops)
+	cnt := atomic.LoadUint64(&cr.cnt)
+	n = int64(cnt & 0xFFFFFFFF)
+	nops = int(cnt >> 32)
+	return
 }
 
 func NewWriteCounter(r io.Writer) WriteCounter {
-	return &CountingWriter{r, 0, 0}
+	return &CountingWriter{r, 0}
 }
 
 type CountingRW struct {

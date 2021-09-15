dee2bde20ec2a3f8f2d90d856bfb5040c5a596f4
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-15 20:03:55 +0530

    [go] Flatten Array iteration 2

--- go/flatten-array/flatten.go
+++ go/flatten-array/flatten.go
@@ -1,31 +1,24 @@
 package flatten
 
-import (
-	"reflect"
-)
-
 // Refer: https://blog.golang.org/laws-of-reflection
+
 // dfs assumes its input as a slice and walks it depth-first
 func dfs(in interface{}, out *[]interface{}) {
-	s := reflect.ValueOf(in)
-	for i := 0; i < s.Len(); i++ {
-		val := s.Index(i)
-		if !val.IsNil() {
-			// s.Index() gives a value of the struct reflect.Value and not the
-			// empty interface, which is what we want, since the input is a
-			// slice of empty interfaces, so doing s.Index().(int) won’t work
-			v := val.Interface()
-			switch reflect.TypeOf(v).Kind() {
-			case reflect.Slice:
+	s := in.([]interface{})
+	for _, e := range s {
+		if e != nil {
+			switch v := e.(type) {
+			case int:
+				*out = append(*out, v)
+			case []interface{}:
 				dfs(v, out)
-			case reflect.Int:
-				*out = append(*out, v.(int))
 			}
 		}
 	}
 }
+
 func Flatten(l interface{}) []interface{} {
-	r := make([]interface{}, 0)
+	r := []interface{}{}
 	dfs(l, &r)
 	return r
 }

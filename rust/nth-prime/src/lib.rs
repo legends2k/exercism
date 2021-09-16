571369a7fd4e7acfb4a82cfebcce74dad81a7536
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 19:01:36 +0530

    [rust] N-th Prime iteration 3
    Memoization optimization to avoid duplication of work in arriving at
    the same prime numbers repeatedly.

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,33 +1,42 @@
-fn is_prime(n: u32, p: &Vec<u32>) -> bool {
-  // Integers larger than the square root do not need to be checked because,
-  // whenever n = a × b, one of the two factors a and b ≤ √n or one of them
-  // needs to be a real number; however for primality we need both to be
-  // integers. So given n, check if any z ∈ [2, ⌊√n⌋] evenly divides n.
-  // OPTIMIZATIONS
-  // 1. Avoid using the usually expensive `sqrt`
-  // 2. Only check prime numbers in the range
-  // REFERENCES
-  // https://en.wikipedia.org/wiki/Prime_number#Trial_division
-  // https://en.wikipedia.org/wiki/Primality_test
-  for factor in p {
-    // break if we’ve exceeded √n
-    if (factor * factor) > n {
-      break;
-    } else if (n % factor) == 0 {
-      return false;
+struct PrimeChecker {
+  primes: Vec<u32>,
+}
+
+impl PrimeChecker {
+  fn is_prime(&mut self, n: u32) -> bool {
+    // Integers larger than the square root do not need to be checked
+    // because, whenever n = a × b, one of the two factors a and b ≤ √n or
+    // one of them  needs to be a real number; however for primality we need
+    // both to be integers. So given n, check if any z ∈ [2, ⌊√n⌋] evenly
+    // divides n.
+    // OPTIMIZATIONS
+    // 1. Avoid using the usually expensive `sqrt`
+    // 2. Only check prime numbers in the range
+    // REFERENCES
+    // https://en.wikipedia.org/wiki/Prime_number#Trial_division
+    // https://en.wikipedia.org/wiki/Primality_test
+    //
+    // NOTE: a generalized PrimeChecker getting non-sequential queries
+    // should first check if n is in primes before remainder checking. Also
+    // if n < primes.last() then skip remainder checking completely.
+    for factor in self.primes.iter() {
+      // break if we’ve exceeded √n
+      if (factor * factor) > n {
+        break;
+      } else if (n % factor) == 0 {
+        return false;
+      }
     }
+    self.primes.push(n);
+    true
   }
-  true
 }
 
 pub fn nth(n: u32) -> u32 {
-  let mut p = vec![2, 3];
-  let mut z = *p.last().unwrap();
-  while n > (p.len() - 1) as u32 {
-    z = z + 1;
-    if is_prime(z, &p) {
-      p.push(z);
-    }
-  }
-  p[n as usize]
+  // NOTE: an optimization would be include first few primes in this LUT
+  let mut checker = PrimeChecker { primes: vec![] };
+  (2..)
+    .filter(|z| checker.is_prime(*z))
+    .nth(n as usize)
+    .unwrap()
 }

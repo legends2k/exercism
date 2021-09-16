18812e450f0d95226deb6f05ad8c7334652a6df2
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 18:58:41 +0530

    [rust] N-th Prime iteration 2
    Basic optimization of not checking above √n

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,6 +1,19 @@
-fn is_prime(n: u32) -> bool {
-  for i in 2..n {
-    if (n % i) == 0 {
+fn is_prime(n: u32, p: &Vec<u32>) -> bool {
+  // Integers larger than the square root do not need to be checked because,
+  // whenever n = a × b, one of the two factors a and b ≤ √n or one of them
+  // needs to be a real number; however for primality we need both to be
+  // integers. So given n, check if any z ∈ [2, ⌊√n⌋] evenly divides n.
+  // OPTIMIZATIONS
+  // 1. Avoid using the usually expensive `sqrt`
+  // 2. Only check prime numbers in the range
+  // REFERENCES
+  // https://en.wikipedia.org/wiki/Prime_number#Trial_division
+  // https://en.wikipedia.org/wiki/Primality_test
+  for factor in p {
+    // break if we’ve exceeded √n
+    if (factor * factor) > n {
+      break;
+    } else if (n % factor) == 0 {
       return false;
     }
   }
@@ -8,11 +21,13 @@ fn is_prime(n: u32) -> bool {
 }
 
 pub fn nth(n: u32) -> u32 {
-  let mut i = 2u32;
-  let mut found = 1;
-  while (n + 1) > found {
-    i = i + 1;
-    found = found + if is_prime(i) { 1 } else { 0 };
+  let mut p = vec![2, 3];
+  let mut z = *p.last().unwrap();
+  while n > (p.len() - 1) as u32 {
+    z = z + 1;
+    if is_prime(z, &p) {
+      p.push(z);
+    }
   }
-  i
+  p[n as usize]
 }

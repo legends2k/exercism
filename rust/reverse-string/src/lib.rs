02b13353fe9187d6debce598412e499d9fb4c31b
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 19:54:54 +0530

    [rust] Reverse String iteration 4

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,11 +1,11 @@
-extern crate unicode_segmentation;
-
 use unicode_segmentation::UnicodeSegmentation;
 
+// see this on why String::chars shouldn’t be used
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=779bfd82f20caf0e588901f3a5051a6d
+
 pub fn reverse(input: &str) -> String {
-  let mut result = String::with_capacity(input.len());
-  for gc in UnicodeSegmentation::graphemes(input, false /*extended*/).rev() {
-    result.push_str(gc)
-  }
-  result
+  input
+    .graphemes(true /*extended*/)
+    .rev()
+    .collect::<String>()
 }

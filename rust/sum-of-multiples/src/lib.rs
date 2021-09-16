433cb697085c1a3f671ba038d22ecedd0148bc28
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 19:27:37 +0530

    [rust] Sum of Multiples iteration 2
    Invert the iterated sequence; iterate over integers within limit and
    look for them in the factors instead of the other way around.

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,10 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-  let mut v: Vec<u32> = factors
-    .iter()
-    .filter(|&f| *f > 0) // step_by can’t be 0
-    .flat_map(|&f| (f..).step_by(f as usize).take_while(|f| *f < limit))
-    .collect();
-  v.sort_unstable();
-  v.dedup();
-  v.iter().sum()
+  (1..limit)
+    .filter(|x| factors.iter().any(|f| (f > &0) && (x % f) == 0))
+    .sum()
 }

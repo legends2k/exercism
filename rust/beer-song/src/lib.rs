6bdb671228c98427784f6cd591ae1302dedbeb7a
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 19:20:54 +0530

    [rust] Beer Song iteration 7
    * Clean up commented code in functions
    * Use iter functions
    * Remove `Phrase` input from plurality_suffix function

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,22 +1,3 @@
-// enum Phrase {
-//     One,
-//     Two,
-//     Three,
-//     Four,
-// }
-
-fn plurality_suffix(n: i32, /*, p: Phrase*/) -> (&'static str, &'static str) {
-  match n {
-    1 => ("", "s"),
-    2 => ("s", ""),
-    _ => ("s", "s"),
-  }
-  // match p {
-  //     Phrase::Three => if n != 2 { "s" } else { "" },
-  //     _ => if n != 1 { "s" } else { "" },
-  // }
-}
-
 fn count(n: i32) -> (String, String, String) {
   let n_str = n.to_string();
   let l_str = (n - 1).to_string();
@@ -31,6 +12,14 @@ fn count(n: i32) -> (String, String, String) {
   }
 }
 
+fn plurality_suffix(n: i32) -> (&'static str, &'static str) {
+  match n {
+    1 => ("", "s"),
+    2 => ("s", ""),
+    _ => ("s", "s"),
+  }
+}
+
 fn phrase3(n: u32) -> String {
   match n {
     0 => String::from("Go to the store and buy some more"),
@@ -42,34 +31,25 @@ fn phrase3(n: u32) -> String {
 }
 
 pub fn verse(n: u32) -> String {
-  //     // line 1
-  //     let count0 = if n >= 1 { n.to_string() } else { String::from("No more") };
-  //     let count1 = if n >= 1 { n.to_string() } else { String::from("no more") };
-  //     let plural0 = if n != 1 { "s" } else { "" };
-  //     let mut v = format!("{0} bottle{2} of beer on the wall, {1} bottle{2} of beer.\n", count0, count1, plural0);
-  //     let count3 = if n != 1 { "one" } else { "it" };
-  //     let count4 = if n > 1 { (n - 1).to_string() } else { String::from("no more") };
-  //     let plural1 = if n != 2 { "s" } else { "" };
-  //     let l2 = match n {
-  //         1..=99 => format!("Take {} down and pass it around, {} bottle{} of beer on the wall.\n", count3, count4, plural1),
-  //         _ => format!("{}", "Go to the store and buy some more, 99 bottles of beer on the wall.\n"),
-  //     };
-  //     v.push_str(&l2);
-  //     v
-  let (c1, c2, c3) = count(n as i32);
-  let (s1, s2) = plurality_suffix(n as i32);
-  let mut v = format!(
-    "{} bottle{} of beer on the wall, {} bottle of beer",
-    c1, s1, c2
-  );
-  // v.push_str(format!("{}, {} bottle{} of beer on the wall.", phrase3(n), c3, s2));
-  v
+  let (count1, count2, count3) = count(n as i32);
+  let (suffix1, suffix2) = plurality_suffix(n as i32);
+  format!(
+    "{} bottle{} of beer on the wall, {} bottle{} of beer.\n\
+     {}, {} bottle{} of beer on the wall.\n",
+    count1,
+    suffix1,
+    count2,
+    suffix1,
+    phrase3(n),
+    count3,
+    suffix2
+  )
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-  let mut v = Vec::with_capacity((start - end) as usize);
-  for x in (end..=start).rev() {
-    v.push(verse(x));
-  }
-  return v.join("\n");
+  (end..=start)
+    .rev()
+    .map(|i| verse(i))
+    .collect::<Vec<String>>()
+    .join("\n")
 }

a9fac759e07d616594dad29945615d30ea309950
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 21:39:04 +0530

    [rust] Atbash Cipher iteration 6
    Mentor feedback on iteration 5 was to implement the iterator approach
    which renders both encode and decode into oneliners.
    
    * Define an iterator struct with a lifetime annotation
    * Implement `next` method returning a `&str`’s contents byte by byte
      as `char`; returns `None` in case it meets a non ASCII alphanumeric
    * Implement CharIter for mapping a char to a char with/out space
      - This returns space when needed or the actual char otherwise
    * Map an iterator to another iterator and use `flat_map` to linearlize
      all the characters including the ones needing space insertion
    
    Both the custom iterator types are unneeded as shown by the mentor [1]
      - Make `atbash_str` do
        `input.chars().filter(...).map(|x| atbash(x.to_lower))`
      - `add_spaces` can do
        `Some(' ').filter(...).into_iter().chain(std::iter::once(x))`
    
    `encode` can also avoid collect by doing
    `String::{with_capacity,extend}`.
    
    [1]: https://exercism.org/tracks/rust/exercises/atbash-cipher/mentor_discussions/75b4e104-08c7-11ec-81c5-853579c25f94

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -7,38 +7,104 @@ fn atbash(data: char) -> char {
   }
 }
 
-/// "Encipher" with the Atbash cipher.
-pub fn encode(plain: &str) -> String {
-  let mut chars = plain
-    .as_bytes()
-    .iter()
-    .filter(|b| b.is_ascii_alphanumeric());
-  let mut out_idx = 0;
-  let iter = std::iter::from_fn(|| {
-    out_idx += 1;
-    if out_idx % 6 == 0 {
-      Some(char::from(b' '))
-    } else {
-      match chars.next() {
-        Some(ch) => Some(atbash(char::from(ch.to_ascii_lowercase()))),
-        None => None,
+struct AtbashIterator<'a> {
+  data: &'a str,
+  idx: usize,
+}
+
+impl Iterator for AtbashIterator<'_> {
+  type Item = char;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    while self.idx < self.data.len() {
+      let ch = self.data.as_bytes()[self.idx] as char;
+      self.idx += 1;
+      if ch.is_ascii_alphanumeric() {
+        return Some(atbash(ch.to_ascii_lowercase()));
       }
     }
-  });
-  // avoid iter.collect() here to prevent multiple allocations
-  let mut cipher = String::with_capacity(plain.len() + plain.len() / 5);
-  cipher.extend(iter);
-  if cipher.ends_with(" ") {
-    cipher.pop();
+    None
   }
-  return cipher;
+
+  fn size_hint(&self) -> (usize, Option<usize>) {
+    (0, Some(self.data.len() - self.idx))
+  }
+}
+
+fn atbash_str(input: &str) -> AtbashIterator {
+  AtbashIterator {
+    data: input,
+    idx: 0,
+  }
+}
+
+// iterator mapping a char to a char with/out space
+struct CharIter {
+  data: char,
+  ins_space: bool,
+  has_ended: bool,
+}
+
+impl CharIter {
+  fn new(ch: char, need_space: bool) -> Self {
+    CharIter {
+      data: ch,
+      ins_space: need_space,
+      has_ended: false,
+    }
+  }
+}
+
+impl Iterator for CharIter {
+  type Item = char;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    match self.has_ended {
+      true => None,
+      false => match self.ins_space {
+        false => {
+          self.has_ended = true;
+          Some(self.data)
+        }
+        true => {
+          self.ins_space = false;
+          Some(' ')
+        }
+      },
+    }
+  }
+
+  fn size_hint(&self) -> (usize, Option<usize>) {
+    match (self.has_ended, self.ins_space) {
+      (true, _) => (0, Some(0)),
+      (false, true) => (2, Some(2)),
+      (false, false) => (1, Some(1)),
+    }
+  }
+}
+
+// When taking an iterator input, prefer IntoIterator over Iterator for
+// cleaner client code
+// https://stackoverflow.com/a/34969944/183120
+// Options to return an iterator.  `impl Trait`: static dispatch.
+// https://stackoverflow.com/a/27535594/183120
+fn add_spaces<It>(input: It) -> impl Iterator<Item = char>
+where
+  It: IntoIterator<Item = char>,
+{
+  input
+    .into_iter() // Iterators return self
+    .enumerate()
+    .flat_map(|(i, x)| CharIter::new(x, i % 5 == 0))
+    .skip(1) // skip leading white space
+}
+
+/// "Encipher" with the Atbash cipher.
+pub fn encode(plain: &str) -> String {
+  add_spaces(atbash_str(plain)).collect()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-  cipher
-    .chars()
-    .filter(|c| c.is_ascii_alphanumeric()) // remove spaces
-    .map(atbash)
-    .collect()
+  atbash_str(cipher).collect()
 }

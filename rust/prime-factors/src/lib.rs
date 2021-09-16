b4965570013fa4a10de1eacc4700a519255f542c
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 19:50:44 +0530

    [rust] Prime Factors iteration 5
    * Use extend instead of append
    * Use iter::repeat instead of constructing a Vec only to throw it away
    * Embellish with comments generously

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,6 +1,8 @@
+use std::iter;
+
 /// Extracts all f-ness is n
-/// e.g. extract all evenness in n by passing (n, 2)
-fn extract(n: &mut u64, f: u64) -> u8 {
+/// e.g. extract all evenness in n by passing `(n, 2)`
+fn extract(n: &mut u64, f: u64) -> u32 {
   let mut count = 0;
   while *n % f == 0 {
     *n /= f;
@@ -9,18 +11,31 @@ fn extract(n: &mut u64, f: u64) -> u8 {
   count
 }
 
+// Basic Idea is to iterate over (2..=n), test for clean divisibility, divide
+// and append factor.  However, once factor 2 iteration is done, all further
+// divisiors/factors will be odd, so idx f can be double incremented.  Special
+// case 2 and do regular iteration from 3 onwards.  Note: we don’t check if f is
+// prime since we’re iterating from 2, we’d’ve already tried to divide n by
+// prime factors some composite/non-prime number is composed of.
+//
+// REFERENCES
+//     https://en.wikipedia.org/wiki/Integer_factorization
+//     https://en.wikipedia.org/wiki/Trial_division
+//     https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
 pub fn factors(n: u64) -> Vec<u64> {
   let mut m = n;
   let i = extract(&mut m, 2);
   let mut v = vec![2; i as usize];
   let mut f = 3;
-  // important optimisation: run only till √n, anything pending will
-  // be a prime number; factors of a number can’t be greater
+  // Optimisation 1: run only till √n as factors of a number can’t be
+  // greater. Anything pending will be a prime number.
   // https://stackoverflow.com/q/5811151/183120
   while (f * f) <= n {
     let i = extract(&mut m, f);
-    v.append(&mut vec![f; i as usize]);
+    v.extend(iter::repeat(f).take(i as usize));
     f += 2;
+    // Optimisation 2: skip even numbers since all evenness
+    // has been extracted from n.
   }
   // we’ve a prime number pending
   if m != 1 {
